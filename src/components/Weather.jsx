db0eda6514b2aa9acab4de583c0d31574a95f21e
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-10-21 15:44:49 +1100

    html

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -4,6 +4,15 @@ export default function Weather() {
   return (
     <main>
       <h1>Todays Weather</h1>
+      <div className="location">
+        <h2>My location: </h2>
+        <p>Wagga Wagga</p>
+      </div>
+      <div className="temperature">
+        <h2>26C</h2>
+        <h3>Sunny</h3>
+        <h4>L:11 / H:26</h4>
+      </div>
     </main>
   );
 }
