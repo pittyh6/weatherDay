07cb5db1cdc0049c4c2077a27add7e8a6b160de0
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-11-26 15:16:01 +1100

    change and follow new weather code with acios

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import WeatherAPI from "./WeatherAPI";
 
-export default function Weather() {
+/*export default function Weather() {
   return (
     <main>
       <WeatherAPI />
@@ -17,4 +18,56 @@ export default function Weather() {
       </div>
     </main>
   );
-}
+}*/
+const Weather = () => {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState("");
+
+  const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+
+  const fetchWeather = async () => {
+    if (!city) {
+      setError("Please enter a city name");
+      return;
+    }
+
+    setError("");
+    try {
+      const response = await axios.get(
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=${API_KEY}&contentType=json`
+      );
+      setWeather(response.data);
+    } catch (err) {
+      setError("Unable to fetch weather data. Please try again.");
+    }
+  };
+
+  return(
+    <div>
+      <h1>Weather Now</h1>
+      <div>
+        <input type="text"
+        placeholder="Enter City Name"
+        value={city} 
+        onChange={(e) => setCity(e.target.value)}/>
+        <button onClick={fetchWeather}>Get Weather</button>
+      </div>
+
+      {error && <p style={{color='red'}}>{error}</p>}
+
+      {weather && (
+        <div>
+          <h2>Weather in {weather.address}</h2>
+          <p>Temperature: {weather.currentConditions.temp} °C</p>
+          <p>Condition: {weather.currentConditions.conditions}</p>
+          <p>Humidity: {weather.currentConditions.humidity}%</p>
+          <p>Wind Speed: {weather.currentConditions.windspeed} km/h</p>
+        </div>
+      )}
+    </div>
+  )
+};
+
+
+export default Weather;
