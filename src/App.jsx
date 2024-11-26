0414c312c27ede6c55d82b7b3ecc3faf2a2534b1
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-11-26 15:25:02 +1100

    fic bug not showing anything

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import React from "react";
 import Weather from "./components/Weather";
 
 function App() {
-  return <Weather />;
+  return (
+    <div>
+      <Weather />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import axios from "axios";
-import WeatherAPI from "./WeatherAPI";
 
 /*export default function Weather() {
   return (
